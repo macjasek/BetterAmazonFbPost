377b8288aa8cac01d618e1f9c013c5d11759215c
macjasek <maciej.dobruchowski@ispro.com.pl>
2017-06-14 16:56:50 +0200

    Import Project

---
+++ BetterAmazonFbPost/Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBoxDomain = New System.Windows.Forms.TextBox()
+        Me.TextBoxTitle = New System.Windows.Forms.TextBox()
+        Me.TextBoxDescription = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Title = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 65)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(800, 418)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(409, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Load Page"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(24, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(359, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 65)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(800, 419)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.Location = New System.Drawing.Point(834, 65)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(201, 355)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(506, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "List Images"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(834, 460)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Save"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(936, 460)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Create Ad"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 65)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(800, 419)
+        Me.RichTextBox1.TabIndex = 8
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.Visible = False
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(834, 490)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Create Html"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBoxDomain
+        '
+        Me.TextBoxDomain.Location = New System.Drawing.Point(102, 513)
+        Me.TextBoxDomain.Name = "TextBoxDomain"
+        Me.TextBoxDomain.Size = New System.Drawing.Size(710, 20)
+        Me.TextBoxDomain.TabIndex = 10
+        '
+        'TextBoxTitle
+        '
+        Me.TextBoxTitle.Location = New System.Drawing.Point(102, 539)
+        Me.TextBoxTitle.Name = "TextBoxTitle"
+        Me.TextBoxTitle.Size = New System.Drawing.Size(710, 20)
+        Me.TextBoxTitle.TabIndex = 11
+        '
+        'TextBoxDescription
+        '
+        Me.TextBoxDescription.Location = New System.Drawing.Point(102, 566)
+        Me.TextBoxDescription.Name = "TextBoxDescription"
+        Me.TextBoxDescription.Size = New System.Drawing.Size(710, 20)
+        Me.TextBoxDescription.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 520)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Domain:"
+        '
+        'Title
+        '
+        Me.Title.AutoSize = True
+        Me.Title.Location = New System.Drawing.Point(12, 546)
+        Me.Title.Name = "Title"
+        Me.Title.Size = New System.Drawing.Size(30, 13)
+        Me.Title.TabIndex = 15
+        Me.Title.Text = "Title:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 573)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 13)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Description:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(599, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 13)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "Image Properties"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1049, 627)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Title)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxDescription)
+        Me.Controls.Add(Me.TextBoxTitle)
+        Me.Controls.Add(Me.TextBoxDomain)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "Better Amazon FB Post"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBoxDomain As TextBox
+    Friend WithEvents TextBoxTitle As TextBox
+    Friend WithEvents TextBoxDescription As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Title As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ BetterAmazonFbPost/Form1.vb
@@ -0,0 +1,106 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        WebBrowser1.Navigate(TextBox1.Text)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim htmlDocument As HtmlDocument = Me.WebBrowser1.Document
+        Dim htmlElementCollection As HtmlElementCollection = htmlDocument.Images
+        For Each htmlElement As HtmlElement In htmlElementCollection
+            Dim imgUrl As String = htmlElement.GetAttribute("src")
+            'If imgUrl.Contains("/ecx") Then
+            ListBox1.Items.Add(imgUrl)
+            'End If
+
+        Next
+        WebBrowser1.Visible = False
+        PictureBox1.Visible = True
+        ListBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        PictureBox1.ImageLocation = ListBox1.SelectedItem
+        If ListBox1.SelectedIndex > 0 Then
+            Label3.Text = PictureBox1.Image.Width
+        End If
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Source As String
+        Dim Destination As String
+        Source = ListBox1.SelectedItem
+        Source = Replace(Source, "SX355", "SX1000")
+        Destination = "C:\temp\img.jpg"
+        If System.IO.File.Exists(Destination) Then System.IO.File.Delete(Destination)
+        My.Computer.Network.DownloadFile(Source, Destination)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If System.IO.File.Exists("C:\temp\blank.png") = False Then
+            Dim NewBitmap As New Bitmap(1200, 628, Imaging.PixelFormat.Format32bppPArgb)
+            Graphics.FromImage(NewBitmap).Clear(Color.White)
+            NewBitmap.Save("C:\temp\blank.png", Imaging.ImageFormat.Png)
+        End If
+        Dim newBM As Bitmap     'the "canvas" to draw on
+        Dim imgBG As Bitmap     'the background image
+        Dim imgBGb As Bitmap    'the black background image
+        Dim imgAmz As Bitmap   'the amazon image
+        imgBG = Image.FromFile("C:\temp\blank.png")
+        imgBGb = Image.FromFile("C:\temp\blank-black.png")
+        imgAmz = Image.FromFile("C:\temp\img.jpg")
+        newBM = New Bitmap(1200, 628)
+        Dim NewHeight As Integer = 628
+        Dim NewWidth As Integer = NewHeight / imgAmz.Height * imgAmz.Width
+
+        If NewWidth > 1200 Then
+            NewWidth = 1200
+            NewHeight = NewWidth / imgAmz.Width * imgAmz.Height
+        End If
+
+        imgAmz = New Bitmap(imgAmz, NewWidth, NewHeight)
+
+        Dim Xpt As Integer = 600 - (NewWidth / 2)
+
+        Graphics.FromImage(newBM).DrawImage(imgBG, 0, 0)
+        Graphics.FromImage(newBM).DrawImage(imgAmz, Xpt, 0)
+        newBM.Save("C:\temp\image01.png", Imaging.ImageFormat.Png)
+        PictureBox1.Image = New Bitmap(newBM)
+        Graphics.FromImage(newBM).DrawImage(imgBGb, 0, 0)
+        Graphics.FromImage(newBM).DrawImage(imgAmz, Xpt, 0)
+        newBM.Save("C:\temp\image02.png", Imaging.ImageFormat.Png)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim Directory As String = TextBox1.Text
+        DisplayMetaDescription()
+    End Sub
+    Private Sub DisplayMetaDescription()
+        If (WebBrowser1.Document IsNot Nothing) Then
+            Dim Elems As HtmlElementCollection
+            Dim WebOC As WebBrowser = WebBrowser1
+
+            Elems = WebOC.Document.GetElementsByTagName("META")
+
+            For Each elem As HtmlElement In Elems
+                Dim NameStr As String = elem.GetAttribute("name")
+
+                If ((NameStr IsNot Nothing) And (NameStr.Length <> 0)) Then
+                    If NameStr.ToLower().Equals("description") Then
+                        Dim ContentStr As String = elem.GetAttribute("content")
+                        'MessageBox.Show("Document: " & WebOC.Url.ToString() & vbCrLf & "Description: " & ContentStr)
+                        TextBoxDescription.Text = ContentStr
+                    End If
+                    If NameStr.ToLower().Equals("title") Then
+                        Dim ContentStr As String = elem.GetAttribute("content")
+                        'MessageBox.Show("Document: " & WebOC.Url.ToString() & vbCrLf & "Title: " & ContentStr)
+                        TextBoxTitle.Text = ContentStr
+                    End If
+                End If
+
+            Next
+        End If
+    End Sub
+End Class
